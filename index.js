0791efbc74461cc5a21efa997c2b3f9da0aa49e8
Ceorll <88160289+Ceorll@users.noreply.github.com>
2022-07-05 12:31:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const https = require('http')
-
-const hostname = '0.0.0.0';
-const port = 61354;
-
-const server = https.createServer((req,res)=> {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Zeet Node');;
-})
-
-
 const express = require("express")
 const cors = require("cors")
 
@@ -19,6 +7,3 @@ app.use(cors())
 
 app.get("/",(req,res) => {res.download("./v.png")})
 app.listen(4000);
-server.listen(port,hostname,() =>{
-    console.log('Server running at https://${hostname}:${port}')
-});
